d1b55cd9881b28473584a581936e6cc9fc7f1da0
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-05-16 23:56:11 +0900

    feat: searchRepositoriesのクエリを定義

--- src/graphql.js
+++ src/graphql.js
@@ -1,5 +1,42 @@
 import gql from 'graphql-tag'
 
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+    search(
+      first: $first
+      after: $after
+      last: $last
+      before: $before
+      query: $query
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      repositoryCount
+      edges {
+        cursor
+        node {
+          __typename
+          ... on Repository {
+            id
+            name
+            url
+            stargazers   {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`
+
 export const ME = gql`
   query me {
     user(login: "KazukiTakeuchi") {
